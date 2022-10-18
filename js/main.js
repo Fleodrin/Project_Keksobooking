60b535f1c65618edd05ff69556cb10d8420e5967
Vladimir Krivitskii <s2179729@iivt.donstu.ru>
2022-10-18 23:10:11 +0300

    Generates data for local server

--- js/main.js
+++ js/main.js
@@ -1,21 +1,86 @@
-//Source: https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+const TYPE_OF_HOUSE = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel'
+];
+const TIME_IN_OUT = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+const HOUSE_FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+const PHOTO_OF_HOUSE = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+];
+const SIMILAR_ADV = 10;
 
-function getRandomNumber(min, max) {
-  if ((min <= 0) || (max <= min)) {
+const getRandomNumber = (min, max) => {
+  if (min < 0 || max < min) {
     return NaN;
   }
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-getRandomNumber(1, 5);
-
-function getRandomCoordinate(min, max, signs) {
-  if (min <= 0 || max <= min || signs <= 0) {
+};
+const getRandomCoordinate = (a, b, signs = 1) => {
+  if (a < 0 || b < 0 || signs <= 0) {
     return NaN;
   }
-  return ((Math.random() * (max - min)) + min).toFixed(signs);
-}
+  const lower = Math.min(a, b);
+  const upper = Math.max(a, b);
+  return ((Math.random() * (upper - lower)) + lower).toFixed(signs);
+};
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
 
+getRandomNumber(1, 5);
 getRandomCoordinate(1, 2, 6);
+
+const randomAvatarNumber = `0${getRandomNumber(1, 10)}`.slice(-2);
+
+const createAuthor = () => ({
+  avatar : `img/avatars/user${randomAvatarNumber}.png`,
+});
+
+const createOffer = (location) => ({
+  title : 'Апартаменты',
+  address : `${location.lat}, ${location.lng}`,
+  price : getRandomNumber(1, 100000),
+  type : getRandomArrayElement(TYPE_OF_HOUSE),
+  rooms : getRandomNumber(1, 15),
+  guests : getRandomNumber(1, 30),
+  checkin : getRandomArrayElement(TIME_IN_OUT),
+  checkout : getRandomArrayElement(TIME_IN_OUT),
+  features : getRandomArrayElement(HOUSE_FEATURES),
+  description : 'Апартаменты для всех и каждого!',
+  photos : getRandomArrayElement(PHOTO_OF_HOUSE),
+});
+
+const createLocation = () => ({
+  lat: getRandomCoordinate(35.65, 35.7, 5),
+  lng: getRandomCoordinate(139.7, 139.8, 5),
+});
+
+const createAdvert = () => {
+  const location = createLocation();
+  return {
+    author : createAuthor(),
+    offer : createOffer(location),
+    location: createLocation(),
+  };
+};
+
+const listAd = Array.from({length: SIMILAR_ADV}, createAdvert);
+
+// eslint-disable-next-line no-console
+console.log(listAd);
