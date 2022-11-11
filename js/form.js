bb22854fcb8aec34301106aa26ef75d42880e2fb
Vladimir Krivitskii <s2179729@iivt.donstu.ru>
2022-11-11 22:14:35 +0300

    Changes in form.js

--- js/form.js
+++ js/form.js
@@ -1,4 +1,18 @@
+const HOUSE_TYPE = {
+  flat: 1000,
+  bungalow: 0,
+  house: 5000,
+  palace: 10000,
+  hotel: 3000
+};
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LENGTH = 100;
+
 const form = document.querySelector('.ad-form');
+const fieldPrice = form.querySelector('#price');
+const fieldType = form.querySelector('#type');
+const fieldRoomNumber = form.querySelector('#room_number');
+const fieldGuestNumber = form.querySelector('#capacity');
 
 const pristine = new Pristine(form, {
   classTo: 'ad-form__element',
@@ -9,27 +23,25 @@ const pristine = new Pristine(form, {
   errorTextClass: 'ad-form__element--invalid'
 });
 
-const validateTitle = (value) => value.length >= 30 && value.length <= 100;
+const validateTitle = (value) => value.length >= MIN_TITLE_LENGTH && value.length <= MAX_TITLE_LENGTH;
+
+const validatePrice = (input) => Number(input) >= HOUSE_TYPE[fieldType.value] && Number(input) <= 100000;
+
+const getPriceError = () => `Цена от ${HOUSE_TYPE[fieldType.value]} до 100000`;
+
+const validateRoomNumber = () => {
+  if (fieldRoomNumber.value === '100' && fieldGuestNumber.value === '0') {
+    return true;
+  } else if (fieldRoomNumber.value >= fieldGuestNumber.value && fieldGuestNumber.value > '0' && fieldRoomNumber.value !== '100') {
+    return true;
+  }
+};
 
 pristine.addValidator(
   form.querySelector('#title'),
   validateTitle,
-  'От 30 до 100 символов'
+  `От ${MIN_TITLE_LENGTH} до ${MAX_TITLE_LENGTH} символов`
 );
-
-const HOUSE_TYPE = {
-  flat: 1000,
-  bungalow: 0,
-  house: 5000,
-  palace: 10000,
-  hotel: 3000
-};
-const fieldPrice = form.querySelector('#price');
-const fieldType = form.querySelector('#type');
-
-const validatePrice = (input) => parseInt(input, 10) >= HOUSE_TYPE[fieldType.value] && parseInt(input, 10) <= 100000;
-
-const getPriceError = () => `Цена от ${HOUSE_TYPE[fieldType.value]} до 100000`;
 
 pristine.addValidator(
   fieldPrice,
@@ -37,23 +49,17 @@ pristine.addValidator(
   getPriceError
 );
 
-fieldType.addEventListener('change', () => pristine.validate(fieldPrice));
-
-form.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  pristine.validate();
-});
-
-const fieldRoomNumber = form.querySelector('#room_number');
-const fieldGuestNumber = form.querySelector('#capacity');
-
-const validateRoomNumber = () => (fieldRoomNumber.value === '100' && fieldGuestNumber.value === '0') ||
-  (fieldRoomNumber.value >= fieldGuestNumber.value && fieldGuestNumber.value > '0' && fieldRoomNumber.value !== '100');
-
 pristine.addValidator(
   fieldRoomNumber,
   validateRoomNumber,
   'Неверное количество комнат'
 );
 
+fieldType.addEventListener('change', () => pristine.validate(fieldPrice));
+
 fieldGuestNumber.addEventListener('change', () => pristine.validate(fieldRoomNumber));
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+});
